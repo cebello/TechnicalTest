7cef3029ee10b3439290748911c4b59f014ff735
CharlyBello <millobello28@gmail.com>
2025-04-30 18:39:34 -0500

    Configuración base de datos

---
+++ Models/Configuration.cs
@@ -0,0 +1,11 @@
+namespace TechnicalTest.Models
+{
+    public class Configuration
+    {
+        protected static string connectionString = "Server=localhost;Database=TechnicalTest;Trusted_Connection=True;Encrypt=false;TrustServerCertificate=True;";
+
+        public Configuration()
+        {
+        }
+    }
+}
